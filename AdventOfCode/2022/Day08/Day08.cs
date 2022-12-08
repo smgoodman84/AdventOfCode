90f46fd6dd0173c8e9b5cc725e567d51f19ada80
Simon Goodman <simon.goodman84@googlemail.com>
2022-12-08 05:18:05 +0000

    2022 Day 8 Part 1

---
+++ AdventOfCode/2022/Day08/Day08.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Shared;
+using AdventOfCode.Shared.FileProcessing;
+using AdventOfCode.Shared.PrimitiveExtensions;
+
+namespace AdventOfCode._2022.Day08
+{
+    public class Day08 : Day
+    {
+        public Day08() : base(2022, 8, "Day08/input_2022_08.txt", "", "")
+        {
+
+        }
+
+
+        private int[][] _treeHeights;
+        private bool[][] _visible;
+
+        public override void Initialise()
+        {
+            _treeHeights = InputLines
+                .Select(r => r.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray())
+                .ToArray();
+
+            _visible = InputLines
+                .Select(r => r.ToCharArray().Select(c => false).ToArray())
+                .ToArray();
+        }
+
+        public override string Part1()
+        {
+            // from left
+            var leftVisible = 0;
+            foreach (var y in Enumerable.Range(0, _treeHeights.Length))
+            {
+                var maxHeight = -1;
+                var visibleCount = 0;
+                foreach (var x in Enumerable.Range(0, _treeHeights[0].Length))
+                {
+                    var height = _treeHeights[y][x];
+                    if (height > maxHeight)
+                    {
+                        Console.WriteLine($"Left {x}{y}");
+                        visibleCount += 1;
+                        maxHeight = height;
+                        _visible[y][x] = true;
+                    }
+                }
+                leftVisible += visibleCount;
+            }
+
+            // from right
+            var rightVisible = 0;
+            foreach (var y in Enumerable.Range(0, _treeHeights.Length))
+            {
+                var maxHeight = -1;
+                var visibleCount = 0;
+                foreach (var x in Enumerable.Range(0, _treeHeights[0].Length).Reverse())
+                {
+                    var height = _treeHeights[y][x];
+                    if (height > maxHeight)
+                    {
+                        Console.WriteLine($"Right {x}{y}");
+                        visibleCount += 1;
+                        maxHeight = height;
+                        _visible[y][x] = true;
+                    }
+                }
+                rightVisible += visibleCount;
+            }
+
+            // from top
+            var topVisible = 0;
+            foreach (var x in Enumerable.Range(0, _treeHeights[0].Length))
+            {
+                var maxHeight = -1;
+                var visibleCount = 0;
+                foreach (var y in Enumerable.Range(0, _treeHeights.Length))
+                {
+                    var height = _treeHeights[y][x];
+                    if (height > maxHeight)
+                    {
+                        Console.WriteLine($"Top {x}{y}");
+                        visibleCount += 1;
+                        maxHeight = height;
+                        _visible[y][x] = true;
+                    }
+                }
+                topVisible += visibleCount;
+            }
+
+            // from bottom
+            var bottomVisible = 0;
+            foreach (var x in Enumerable.Range(0, _treeHeights[0].Length))
+            {
+                var maxHeight = -1;
+                var visibleCount = 0;
+                foreach (var y in Enumerable.Range(0, _treeHeights.Length).Reverse())
+                {
+                    var height = _treeHeights[y][x];
+                    if (height > maxHeight)
+                    {
+                        Console.WriteLine($"Bottom {x}{y}");
+                        visibleCount += 1;
+                        maxHeight = height;
+                        _visible[y][x] = true;
+                    }
+                }
+                bottomVisible += visibleCount;
+            }
+
+            var totalVisible = _visible
+                .SelectMany(r => r)
+                .Sum(visible => visible ? 1 : 0);
+
+            return totalVisible.ToString();
+        }
+
+        public override string Part2()
+        {
+            return "";
+        }
+    }
+}
+
