b0edca486d165f2cd7367a81837cde79df479520
Simon Goodman <simon.goodman84@googlemail.com>
2022-12-21 06:00:01 +0000

    2022 Day 21 Part 2

--- AdventOfCode/2022/Day21/Day21.cs
+++ AdventOfCode/2022/Day21/Day21.cs
@@ -9,7 +9,7 @@ namespace AdventOfCode._2022.Day21
 {
     public class Day21 : Day
     {
-        public Day21() : base(2022, 21, "Day21/input_2022_21.txt", "85616733059734", "")
+        public Day21() : base(2022, 21, "Day21/input_2022_21.txt", "85616733059734", "3560324848168")
         {
 
         }
@@ -34,19 +34,25 @@ namespace AdventOfCode._2022.Day21
 
         public override string Part2()
         {
-            return "";
+            var root = _lookup["root"] as Root;
+            var humanNeedsToSay = root.FindHumanValueToProduce(0);
+            return humanNeedsToSay.ToString();
         }
 
         private interface IExpression
         {
-
             public string Name { get; }
 
             public long Evaluate();
+
+            public bool ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce);
         }
 
         private IExpression CreateExpression(string name, string expression)
         {
+
             if (long.TryParse(expression, out var constant))
             {
                 return new Constant(name, constant);
@@ -55,6 +61,11 @@ namespace AdventOfCode._2022.Day21
             var split = expression.Split(" ");
             var left = new ExpressionWrapper(split[0], _lookup);
             var right = new ExpressionWrapper(split[2], _lookup);
+
+            if (name == "root")
+            {
+                return new Root("root", left, right);
+            }
 
             switch (split[1])
             {
@@ -67,11 +78,54 @@ namespace AdventOfCode._2022.Day21
             throw new Exception($"Failed to create expression {expression}");
         }
 
+        private class Root : IExpression
+        {
+            private readonly IExpression _left;
+            private readonly IExpression _right;
+
+            public string Name { get; }
+
+            public Root(string name, IExpression left, IExpression right)
+            {
+                Name = name;
+                _left = left;
+                _right = right;
+            }
+
+            public long Evaluate()
+            {
+                return _left.Evaluate() + _right.Evaluate();
+            }
+
+            public bool ReliesOnHuman() => _left.ReliesOnHuman() || _right.ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (_left.ReliesOnHuman() && _right.ReliesOnHuman())
+                {
+                    throw new Exception("Hmmmmm");
+                }
+
+                if (_left.ReliesOnHuman())
+                {
+                    var leftNeedsToEqual = _right.Evaluate();
+                    return _left.FindHumanValueToProduce(leftNeedsToEqual);
+                }
+
+                var rightNeedsToEqual = _left.Evaluate();
+                return _right.FindHumanValueToProduce(rightNeedsToEqual);
+            }
+        }
+
         private class ExpressionWrapper : IExpression
         {
             private readonly Dictionary<string, IExpression> _lookup;
             private bool _evaluated;
             private long _value;
+            private bool _evaluatedReliesOnHuman;
+            private bool _reliesOnHumanValue;
+            private bool _evaluatedNeedToProduce;
+            private long _needToProduceValue;
 
             public ExpressionWrapper(string name, Dictionary<string, IExpression> lookup)
             {
@@ -91,6 +145,28 @@ namespace AdventOfCode._2022.Day21
 
                 return _value;
             }
+
+            public bool ReliesOnHuman()
+            {
+                if (!_evaluatedReliesOnHuman)
+                {
+                    _reliesOnHumanValue = _lookup[Name].ReliesOnHuman();
+                    _evaluatedReliesOnHuman = true;
+                }
+
+                return _reliesOnHumanValue;
+            }
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (!_evaluatedNeedToProduce)
+                {
+                    _needToProduceValue = _lookup[Name].FindHumanValueToProduce(needToProduce);
+                    _evaluatedNeedToProduce = true;
+                }
+
+                return _needToProduceValue;
+            }
         }
 
         private class Constant : IExpression
@@ -108,6 +184,18 @@ namespace AdventOfCode._2022.Day21
             public long Evaluate()
             {
                 return _value;
+            }
+
+            public bool ReliesOnHuman() => Name == "humn";
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (Name != "humn")
+                {
+                    throw new Exception("Hmmmmm, should be humn");
+                }
+
+                return needToProduce;
             }
         }
 
@@ -129,6 +217,25 @@ namespace AdventOfCode._2022.Day21
             {
                 return _left.Evaluate() + _right.Evaluate();
             }
+
+            public bool ReliesOnHuman() => _left.ReliesOnHuman() || _right.ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (_left.ReliesOnHuman() && _right.ReliesOnHuman())
+                {
+                    throw new Exception("Hmmmmm");
+                }
+
+                if (_left.ReliesOnHuman())
+                {
+                    var leftNeedsToEqual = needToProduce - _right.Evaluate();
+                    return _left.FindHumanValueToProduce(leftNeedsToEqual);
+                }
+
+                var rightNeedsToEqual = needToProduce - _left.Evaluate();
+                return _right.FindHumanValueToProduce(rightNeedsToEqual);
+            }
         }
 
         private class Subtract : IExpression
@@ -148,6 +255,25 @@ namespace AdventOfCode._2022.Day21
             public long Evaluate()
             {
                 return _left.Evaluate() - _right.Evaluate();
+            }
+
+            public bool ReliesOnHuman() => _left.ReliesOnHuman() || _right.ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (_left.ReliesOnHuman() && _right.ReliesOnHuman())
+                {
+                    throw new Exception("Hmmmmm");
+                }
+
+                if (_left.ReliesOnHuman())
+                {
+                    var leftNeedsToEqual = needToProduce + _right.Evaluate();
+                    return _left.FindHumanValueToProduce(leftNeedsToEqual);
+                }
+
+                var rightNeedsToEqual = _left.Evaluate() - needToProduce;
+                return _right.FindHumanValueToProduce(rightNeedsToEqual);
             }
         }
 
@@ -169,6 +295,25 @@ namespace AdventOfCode._2022.Day21
             {
                 return _left.Evaluate() * _right.Evaluate();
             }
+
+            public bool ReliesOnHuman() => _left.ReliesOnHuman() || _right.ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (_left.ReliesOnHuman() && _right.ReliesOnHuman())
+                {
+                    throw new Exception("Hmmmmm");
+                }
+
+                if (_left.ReliesOnHuman())
+                {
+                    var leftNeedsToEqual = needToProduce / _right.Evaluate();
+                    return _left.FindHumanValueToProduce(leftNeedsToEqual);
+                }
+
+                var rightNeedsToEqual = needToProduce / _left.Evaluate();
+                return _right.FindHumanValueToProduce(rightNeedsToEqual);
+            }
         }
 
         private class Divide : IExpression
@@ -188,6 +333,25 @@ namespace AdventOfCode._2022.Day21
             public long Evaluate()
             {
                 return _left.Evaluate() / _right.Evaluate();
+            }
+
+            public bool ReliesOnHuman() => _left.ReliesOnHuman() || _right.ReliesOnHuman();
+
+            public long FindHumanValueToProduce(long needToProduce)
+            {
+                if (_left.ReliesOnHuman() && _right.ReliesOnHuman())
+                {
+                    throw new Exception("Hmmmmm");
+                }
+
+                if (_left.ReliesOnHuman())
+                {
+                    var leftNeedsToEqual = needToProduce * _right.Evaluate();
+                    return _left.FindHumanValueToProduce(leftNeedsToEqual);
+                }
+
+                var rightNeedsToEqual = _left.Evaluate() / needToProduce;
+                return _right.FindHumanValueToProduce(rightNeedsToEqual);
             }
         }
     }
