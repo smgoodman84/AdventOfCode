338f54953d97eada229f99f4136bfa9a129053e0
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-02 05:41:06 +0000

    2023 Day 02 Refactor

--- AdventOfCode/2023/Day02/Day02.cs
+++ AdventOfCode/2023/Day02/Day02.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode._2023.Day02
 {
     public class Day02 : Day
     {
-        public Day02() : base(2023, 2, "Day02/input_2023_02.txt", "55538", "54875")
+        public Day02() : base(2023, 2, "Day02/input_2023_02.txt", "2101", "58269")
         {
 
         }
@@ -12,15 +12,17 @@ namespace AdventOfCode._2023.Day02
         private List<Game> _games;
         public override void Initialise()
         {
-            _games = InputLines.Select(Parse).ToList();
+            _games = InputLines
+                .Select(l => new Game(l))
+                .ToList();
         }
 
         public override string Part1()
         {
             var possibleGames = _games
-                .Where(g => !g.Draws.Any(d => d.ColourCounts.ContainsKey("red") && d.ColourCounts["red"] > 12))
-                .Where(g => !g.Draws.Any(d => d.ColourCounts.ContainsKey("green") && d.ColourCounts["green"] > 13))
-                .Where(g => !g.Draws.Any(d => d.ColourCounts.ContainsKey("blue") && d.ColourCounts["blue"] > 14))
+                .Where(g => g.IsPossibleWith("red", 12))
+                .Where(g => g.IsPossibleWith("green", 13))
+                .Where(g => g.IsPossibleWith("blue", 14))
                 .ToList();
 
             var sum = possibleGames.Sum(g => g.GameNuber);
@@ -30,84 +32,77 @@ namespace AdventOfCode._2023.Day02
 
         public override string Part2()
         {
-            var result = _games.Sum(GetPower);
+            var result = _games.Sum(g => g.GetPower());
 
             return result.ToString();
-        }
-
-        private static int GetPower(Game game)
-        {
-            var minimumCubes = new Dictionary<string, int>();
-            foreach(var draw in game.Draws)
-            {
-                foreach(var colour in draw.ColourCounts)
-                {
-                    if (!minimumCubes.ContainsKey(colour.Key))
-                    {
-                        minimumCubes.Add(colour.Key, colour.Value);
-                    }
-                    else
-                    {
-                        if (minimumCubes[colour.Key] < colour.Value)
-                        {
-                            minimumCubes[colour.Key] = colour.Value;
-                        }
-                    }
-                }
-            }
-
-            var power = 1;
-            foreach (var colour in minimumCubes)
-            {
-                power *= colour.Value;
-            }
-            return power;
-        }
-
-        private static Game Parse(string line)
-        {
-            var gameSplit = line.Split(":");
-            var gameNumber = int.Parse(gameSplit[0].Replace("Game ", ""));
-            var game = new Game
-            {
-                GameNuber = gameNumber,
-                Draws = new List<Draw>(),
-            };
-
-            Console.WriteLine($"Game {game.GameNuber}");
-            var draws = gameSplit[1].Split(";", StringSplitOptions.TrimEntries);
-            foreach (var drawDescription in draws)
-            {
-                Console.WriteLine($"{drawDescription}");
-                var colours = drawDescription.Split(",", StringSplitOptions.TrimEntries);
-                var draw = new Draw
-                {
-                    ColourCounts = new Dictionary<string, int>()
-                };
-
-                foreach (var colour in colours)
-                {
-                    Console.WriteLine($"{colour}");
-                    var countSplit = colour.Split(" ");
-                    Console.WriteLine($"{countSplit[1]} ({countSplit[0]})");
-                    draw.ColourCounts.Add(countSplit[1], int.Parse(countSplit[0]));
-                }
-
-                game.Draws.Add(draw);
-            }
-
-            return game;
         }
 
         private class Game
         {
             public int GameNuber { get; set; }
-            public List<Draw> Draws { get; set; }
+            public List<Draw> Draws { get; set; } = new List<Draw>();
+
+            public Game(string line)
+            {
+                var gameSplit = line.Split(":");
+
+                GameNuber = int.Parse(gameSplit[0].Replace("Game ", ""));
+
+                var draws = gameSplit[1].Split(";", StringSplitOptions.TrimEntries);
+                foreach (var drawDescription in draws)
+                {
+                    var draw = new Draw();
+
+                    var colours = drawDescription.Split(",", StringSplitOptions.TrimEntries);
+                    foreach (var colour in colours)
+                    {
+                        var countSplit = colour.Split(" ");
+                        draw.ColourCounts.Add(countSplit[1], int.Parse(countSplit[0]));
+                    }
+
+                    Draws.Add(draw);
+                }
+            }
+
+            public bool IsPossibleWith(string colour, int count)
+            {
+                return !Draws.Any(d => d.ColourCounts.ContainsKey(colour) && d.ColourCounts[colour] > count);
+            }
+
+            public int GetPower()
+            {
+                var minimumCubes = new Dictionary<string, int>();
+                foreach (var draw in Draws)
+                {
+                    foreach (var colour in draw.ColourCounts)
+                    {
+                        if (!minimumCubes.ContainsKey(colour.Key))
+                        {
+                            minimumCubes.Add(colour.Key, colour.Value);
+                        }
+                        else
+                        {
+                            if (minimumCubes[colour.Key] < colour.Value)
+                            {
+                                minimumCubes[colour.Key] = colour.Value;
+                            }
+                        }
+                    }
+                }
+
+                var power = 1;
+                foreach (var colour in minimumCubes)
+                {
+                    power *= colour.Value;
+                }
+
+                return power;
+            }
         }
 
         private class Draw
         {
-            public Dictionary<string, int> ColourCounts { get; set; }
+            public Dictionary<string, int> ColourCounts { get; set; } = new Dictionary<string, int>();
         }
     }
 }
