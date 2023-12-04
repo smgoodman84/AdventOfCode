cd2caf2c600b6186c9bc438227e96d4ac1da7f8f
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-04 05:16:20 +0000

    2023 Day 04 Part 1

---
+++ AdventOfCode/2023/Day04/Day04.cs
@@ -0,0 +1,62 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCode._2023.Day04
+{
+    public class Day04 : Day
+    {
+        public Day04() : base(2023, 4, "Day04/input_2023_04.txt", "", "")
+        {
+
+        }
+
+        private List<ScratchCard> _scratchCards;
+        public override void Initialise()
+        {
+            _scratchCards = InputLines
+                .Select(l => new ScratchCard(l))
+                .ToList();
+        }
+
+        public override string Part1()
+        {
+            var totalPoints = _scratchCards
+                .Sum(sc => sc.Points());
+
+            return totalPoints.ToString();
+        }
+
+        public override string Part2()
+        {
+            return string.Empty;
+        }
+
+        private class ScratchCard
+        {
+            public int CardNumber { get; private set; }
+            public List<int> Numbers { get; private set; }
+            public List<int> WinningNumbers { get; private set; }
+            public ScratchCard(string line)
+            {
+                var gameSplit = line.Split(":", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                CardNumber = int.Parse(gameSplit[0].Replace("Card", "").Trim());
+
+                var numbersSplit = gameSplit[1].Split("|", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                Numbers = numbersSplit[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                WinningNumbers = numbersSplit[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            }
+
+            public int Points()
+            {
+                var winningNumberCount = Numbers.Count(WinningNumbers.Contains);
+
+                if (winningNumberCount == 0)
+                {
+                    return 0;
+                }
+
+                return (int)Math.Pow(2, winningNumberCount - 1);
+            }
+        }
+    }
+}
+
