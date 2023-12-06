8eeb22e9fdd012fe49beabae77688a4c9c1515a7
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-06 07:06:06 +0000

    2023 Day 07 Prepare

---
+++ AdventOfCode/2023/Day07/Day07.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCode._2023.Day07
+{
+    public class Day07 : Day
+    {
+        public Day07() : base(2023, 7, "Day07/input_2023_07.txt", "", "")
+        {
+
+        }
+
+        public override void Initialise()
+        {
+        }
+
+        public override string Part1()
+        {
+            return string.Empty;
+        }
+
+        public override string Part2()
+        {
+            return string.Empty;
+        }
+    }
+}
+
