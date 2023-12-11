ce140212c4f7bc6c5cbc0b9e2c0ec42896519bb9
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-11 06:09:27 +0000

    2023 Day 11 Tidy Up

--- AdventOfCode/2023/Day11/Day11.cs
+++ AdventOfCode/2023/Day11/Day11.cs
@@ -5,20 +5,19 @@ namespace AdventOfCode._2023.Day11
 {
     public class Day11 : Day
     {
-        public Day11() : base(2023, 11, "Day11/input_2023_11.txt", "9684228", "483844716556", true)
+        public Day11() : base(2023, 11, "Day11/input_2023_11.txt", "9684228", "483844716556", false)
         {
 
         }
 
-        private Grid2D<Space> _map;
         private List<Space> _galaxies;
-        private List<long> _emptyRows;
-        private List<long> _emptyColumns;
+        private Dictionary<long, int> _previousEmptyRows;
+        private Dictionary<long, int> _previousEmptyColumns;
         public override void Initialise()
         {
             var galaxyNumber = 1;
             _galaxies = new List<Space>();
-            _map = new Grid2D<Space>(InputLines[0].Length, InputLines.Count);
+            var map = new Grid2D<Space>(InputLines[0].Length, InputLines.Count);
             {
                 var y = 0;
                 foreach (var line in InputLines)
@@ -30,12 +29,12 @@ namespace AdventOfCode._2023.Day11
                         {
                             var galaxy = new Space(galaxyNumber, new Coordinate2D(x, y));
                             _galaxies.Add(galaxy);
-                            _map.Write(x, y, galaxy);
+                            map.Write(x, y, galaxy);
                             galaxyNumber += 1;
                         }
                         else
                         {
-                            _map.Write(x, y, new Space(null, new Coordinate2D(x, y)));
+                            map.Write(x, y, new Space(null, new Coordinate2D(x, y)));
                         }
 
                         x += 1;
@@ -44,60 +43,39 @@ namespace AdventOfCode._2023.Day11
                 }
             }
 
-            _emptyRows = new List<long>();
-            for (var currentY = _map.MinY; currentY <= _map.MaxY; currentY++)
+            _previousEmptyRows = new Dictionary<long, int>();
+            var emptyRows = 0;
+            for (var currentY = map.MinY; currentY <= map.MaxY; currentY++)
             {
-                if (_map.ReadRow(currentY).All(s => !s.IsGalaxy))
+                _previousEmptyRows.Add(currentY, emptyRows);
+                if (map.ReadRow(currentY).All(s => !s.IsGalaxy))
                 {
                     TraceLine($"Row {currentY} Empty");
-                    _emptyRows.Add(currentY);
+                    emptyRows += 1;
                 }
             }
 
-            _emptyColumns = new List<long>();
-            for (var currentX = _map.MinX; currentX <= _map.MaxX; currentX++)
+            _previousEmptyColumns = new Dictionary<long, int>();
+            var emptyColumns = 0;
+            for (var currentX = map.MinX; currentX <= map.MaxX; currentX++)
             {
-                if (_map.ReadColumn(currentX).All(s => !s.IsGalaxy))
+                _previousEmptyColumns.Add(currentX, emptyColumns);
+                if (map.ReadColumn(currentX).All(s => !s.IsGalaxy))
                 {
                     TraceLine($"Column {currentX} Empty");
-                    _emptyColumns.Add(currentX);
+                    emptyColumns += 1;
                 }
             }
         }
 
-        public override string Part1()
+        private long GetShortestPathsAfterExpansions(long expansionCount)
         {
+            var newLocations = new Dictionary<int, Coordinate2D>();
             foreach (var galaxy in _galaxies)
             {
-                var newX = galaxy.Location.X + _emptyColumns.Count(c => c < galaxy.Location.X);
-                var newY = galaxy.Location.Y + _emptyRows.Count(c => c < galaxy.Location.Y);
-                galaxy.NewLocation = new Coordinate2D(newX, newY);
-            }
-
-            var totalShortestPath = 0L;
-            foreach(var g1 in _galaxies)
-            {
-                foreach (var g2 in _galaxies)
-                {
-                    if (g1.GalaxyNumber < g2.GalaxyNumber)
-                    {
-                        var shortestPath = g1.NewLocation.ManhattanDistanceTo(g2.NewLocation);
-                        // TraceLine($"{g1.GalaxyNumber} -> {g2.GalaxyNumber}; {g1.NewLocation} -> {g2.NewLocation} = {shortestPath}");
-                        totalShortestPath += shortestPath;
-                    }
-                }
-            }
-
-            return totalShortestPath.ToString();
-        }
-
-        public override string Part2()
-        {
-            foreach (var galaxy in _galaxies)
-            {
-                var newX = galaxy.Location.X + (_emptyColumns.Count(c => c < galaxy.Location.X) * 999_999L);
-                var newY = galaxy.Location.Y + (_emptyRows.Count(c => c < galaxy.Location.Y) * 999_999L);
-                galaxy.NewLocationPart2 = new Coordinate2D(newX, newY);
+                var newX = galaxy.Location.X + (_previousEmptyColumns[galaxy.Location.X] * expansionCount);
+                var newY = galaxy.Location.Y + (_previousEmptyRows[galaxy.Location.Y] * expansionCount);
+                newLocations.Add(galaxy.GalaxyNumber.Value, new Coordinate2D(newX, newY));
             }
 
             var totalShortestPath = 0L;
@@ -107,14 +85,28 @@ namespace AdventOfCode._2023.Day11
                 {
                     if (g1.GalaxyNumber < g2.GalaxyNumber)
                     {
-                        var shortestPath = g1.NewLocationPart2.ManhattanDistanceTo(g2.NewLocationPart2);
-                        // TraceLine($"{g1.GalaxyNumber} -> {g2.GalaxyNumber}; {g1.NewLocationPart2} -> {g2.NewLocationPart2} = {shortestPath}");
+                        var g1Location = newLocations[g1.GalaxyNumber.Value];
+                        var g2Location = newLocations[g2.GalaxyNumber.Value];
+                        var shortestPath = g1Location.ManhattanDistanceTo(g2Location);
+
+                        TraceLine($"{g1.GalaxyNumber} -> {g2.GalaxyNumber}; {g1Location} -> {g2Location} = {shortestPath}");
                         totalShortestPath += shortestPath;
                     }
                 }
             }
 
-            return totalShortestPath.ToString();
+            return totalShortestPath;
+        }
+
+        public override string Part1()
+        {
+            return GetShortestPathsAfterExpansions(1).ToString();
+        }
+
+        public override string Part2()
+        {
+
+            return GetShortestPathsAfterExpansions(999_999).ToString();
         }
 
         private class Space
@@ -127,8 +119,6 @@ namespace AdventOfCode._2023.Day11
 
             public int? GalaxyNumber { get; }
             public Coordinate2D Location { get; }
-            public Coordinate2D NewLocation { get; set; }
-            public Coordinate2D NewLocationPart2 { get; set; }
 
             public bool IsGalaxy => GalaxyNumber.HasValue;
         }
