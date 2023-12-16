9de162ffd9afbfefa70786cb5b88b05504723ce5
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-16 06:52:44 +0000

    2023 Day 16 Part 2

--- AdventOfCode/2023/Day16/Day16.cs
+++ AdventOfCode/2023/Day16/Day16.cs
@@ -5,20 +5,22 @@ namespace AdventOfCode._2023.Day16
 {
     public class Day16 : Day
     {
-        public Day16() : base(2023, 16, "Day16/input_2023_16.txt", "8098", "", false)
+        public Day16() : base(2023, 16, "Day16/input_2023_16.txt", "8098", "8335", false)
         {
 
         }
 
-        private Grid2D<Location> _map;
-        private HashSet<string> _followed = new HashSet<string>();
         public override void Initialise()
         {
+        }
+
+        private Map LoadMap()
+        {
             var height = InputLines.Count;
-            _map = new Grid2D<Location>(InputLines[0].Length, height);
-            foreach (var y in _map.YIndexes())
+            var map = new Grid2D<Location>(InputLines[0].Length, height);
+            foreach (var y in map.YIndexes())
             {
-                foreach (var x in _map.XIndexes())
+                foreach (var x in map.XIndexes())
                 {
                     var location = new Location
                     {
@@ -26,147 +28,218 @@ namespace AdventOfCode._2023.Day16
                         EnergizedCount = 0
                     };
 
-                    _map.Write(x, y, location);
+                    map.Write(x, y, location);
                 }
             }
+
+            return new Map(map);
         }
 
         public override string Part1()
         {
-            FollowBeam(new Coordinate2D(0, _map.MaxY), Direction.Left);
-
-            var energizedCount = _map.ReadAll().Count(x => x.EnergizedCount > 0);
-
-
-            foreach (var y in _map.YIndexes().OrderByDescending(y => y))
-            {
-                foreach (var x in _map.XIndexes())
-                {
-                    var location = _map.Read(x, y);
-                    Trace(location.EnergizedCount > 0 ? '#' : '.');
-                }
-                TraceLine();
-            }
+            var map = LoadMap();
+            var energizedCount = map.GetEnergizedCount(new Coordinate2D(0, map._map.MaxY), Direction.Left);
 
             return energizedCount.ToString();
         }
 
-        private void FollowBeam(Coordinate2D coordinate, Direction entersFrom)
-        {
-            if (!_map.IsInGrid(coordinate))
-            {
-                TraceLine($"Left the grid from {entersFrom} at {coordinate}");
-                return;
-            }
-
-            var description = $"{coordinate} {entersFrom}";
-            if (_followed.Contains(description))
-            {
-                TraceLine($"Already tracing from {entersFrom} at {coordinate}");
-                return;
-            }
-            _followed.Add(description);
-
-            var location = _map.Read(coordinate);
-            location.EnergizedCount += 1;
-            TraceLine($"Energized {coordinate}");
-
-            var up = coordinate.Up();
-            var down = coordinate.Down();
-            var left = coordinate.Left();
-            var right = coordinate.Right();
-
-            switch (location.Type)
-            {
-                case '.':
-                    switch (entersFrom)
-                    {
-                        case Direction.Left:
-                            FollowBeam(right, Direction.Left);
-                            break;
-                        case Direction.Right:
-                            FollowBeam(left, Direction.Right);
-                            break;
-                        case Direction.Up:
-                            FollowBeam(down, Direction.Up);
-                            break;
-                        case Direction.Down:
-                            FollowBeam(up, Direction.Down);
-                            break;
-                    }
-                    break;
-                case '/':
-                    switch (entersFrom)
-                    {
-                        case Direction.Left:
-                            FollowBeam(up, Direction.Down);
-                            break;
-                        case Direction.Right:
-                            FollowBeam(down, Direction.Up);
-                            break;
-                        case Direction.Up:
-                            FollowBeam(left, Direction.Right);
-                            break;
-                        case Direction.Down:
-                            FollowBeam(right, Direction.Left);
-                            break;
-                    }
-                    break;
-                case '\\':
-                    switch (entersFrom)
-                    {
-                        case Direction.Left:
-                            FollowBeam(down, Direction.Up);
-                            break;
-                        case Direction.Right:
-                            FollowBeam(up, Direction.Down);
-                            break;
-                        case Direction.Up:
-                            FollowBeam(right, Direction.Left);
-                            break;
-                        case Direction.Down:
-                            FollowBeam(left, Direction.Right);
-                            break;
-                    }
-                    break;
-                case '-':
-                    switch (entersFrom)
-                    {
-                        case Direction.Left:
-                            FollowBeam(right, Direction.Left);
-                            break;
-                        case Direction.Right:
-                            FollowBeam(left, Direction.Right);
-                            break;
-                        case Direction.Up:
-                        case Direction.Down:
-                            FollowBeam(left, Direction.Right);
-                            FollowBeam(right, Direction.Left);
-                            break;
-                    }
-                    break;
-                case '|':
-                    switch (entersFrom)
-                    {
-                        case Direction.Left:
-                        case Direction.Right:
-                            FollowBeam(down, Direction.Up);
-                            FollowBeam(up, Direction.Down);
-                            break;
-                        case Direction.Up:
-                            FollowBeam(down, Direction.Up);
-                            break;
-                        case Direction.Down:
-                            FollowBeam(up, Direction.Down);
-                            break;
-                    }
-                    break;
-            }
-        }
-
         public override string Part2()
         {
-            return string.Empty;
+            var baseMap = LoadMap()._map;
+            var maxEnergizedCount = 0;
+            foreach(var y in baseMap.YIndexes())
+            {
+                var map = LoadMap();
+                var energizedCount = map.GetEnergizedCount(new Coordinate2D(0, y), Direction.Left);
+                if (energizedCount > maxEnergizedCount)
+                {
+                    TraceLine($"{energizedCount} (0, {y}) Left - New Max");
+                    maxEnergizedCount = energizedCount;
+                }
+                else
+                {
+                    TraceLine($"{energizedCount} (0, {y}) Left");
+                }
+            }
+
+            foreach (var y in baseMap.YIndexes())
+            {
+                var map = LoadMap();
+                var energizedCount = map.GetEnergizedCount(new Coordinate2D(baseMap.MaxX, y), Direction.Right);
+                if (energizedCount > maxEnergizedCount)
+                {
+                    TraceLine($"{energizedCount} ({baseMap.MaxX}, {y}) Right - New Max");
+                    maxEnergizedCount = energizedCount;
+                }
+                else
+                {
+                    TraceLine($"{energizedCount} ({baseMap.MaxX}, {y}) Right");
+                }
+            }
+
+            foreach (var x in baseMap.XIndexes())
+            {
+                var map = LoadMap();
+                var energizedCount = map.GetEnergizedCount(new Coordinate2D(x, 0), Direction.Down);
+                if (energizedCount > maxEnergizedCount)
+                {
+                    TraceLine($"{energizedCount} ({x}, 0) Down - New Max");
+                    maxEnergizedCount = energizedCount;
+                }
+                else
+                {
+                    TraceLine($"{energizedCount} ({x}, 0) Down");
+                }
+            }
+
+            foreach (var x in baseMap.XIndexes())
+            {
+                var map = LoadMap();
+                var energizedCount = map.GetEnergizedCount(new Coordinate2D(x, baseMap.MaxY), Direction.Up);
+                if (energizedCount > maxEnergizedCount)
+                {
+                    TraceLine($"{energizedCount} ({x}, {baseMap.MaxY}) Up - New Max");
+                    maxEnergizedCount = energizedCount;
+                }
+                else
+                {
+                    TraceLine($"{energizedCount} ({x}, {baseMap.MaxY}) Up");
+                }
+            }
+
+            return maxEnergizedCount.ToString();
+        }
+
+
+        private class Map
+        {
+            public Grid2D<Location> _map;
+            private HashSet<string> _followed = new HashSet<string>();
+
+            public Map(Grid2D<Location> map)
+            {
+                _map = map;
+            }
+
+            public int GetEnergizedCount(Coordinate2D start, Direction entersFrom)
+            {
+
+                FollowBeam(start, entersFrom);
+
+                var energizedCount = _map.ReadAll().Count(x => x.EnergizedCount > 0);
+
+                return energizedCount;
+            }
+
+            private void FollowBeam(Coordinate2D coordinate, Direction entersFrom)
+            {
+                if (!_map.IsInGrid(coordinate))
+                {
+                    return;
+                }
+
+                var description = $"{coordinate} {entersFrom}";
+                if (_followed.Contains(description))
+                {
+                    return;
+                }
+                _followed.Add(description);
+
+                var location = _map.Read(coordinate);
+                location.EnergizedCount += 1;
+
+                var up = coordinate.Up();
+                var down = coordinate.Down();
+                var left = coordinate.Left();
+                var right = coordinate.Right();
+
+                switch (location.Type)
+                {
+                    case '.':
+                        switch (entersFrom)
+                        {
+                            case Direction.Left:
+                                FollowBeam(right, Direction.Left);
+                                break;
+                            case Direction.Right:
+                                FollowBeam(left, Direction.Right);
+                                break;
+                            case Direction.Up:
+                                FollowBeam(down, Direction.Up);
+                                break;
+                            case Direction.Down:
+                                FollowBeam(up, Direction.Down);
+                                break;
+                        }
+                        break;
+                    case '/':
+                        switch (entersFrom)
+                        {
+                            case Direction.Left:
+                                FollowBeam(up, Direction.Down);
+                                break;
+                            case Direction.Right:
+                                FollowBeam(down, Direction.Up);
+                                break;
+                            case Direction.Up:
+                                FollowBeam(left, Direction.Right);
+                                break;
+                            case Direction.Down:
+                                FollowBeam(right, Direction.Left);
+                                break;
+                        }
+                        break;
+                    case '\\':
+                        switch (entersFrom)
+                        {
+                            case Direction.Left:
+                                FollowBeam(down, Direction.Up);
+                                break;
+                            case Direction.Right:
+                                FollowBeam(up, Direction.Down);
+                                break;
+                            case Direction.Up:
+                                FollowBeam(right, Direction.Left);
+                                break;
+                            case Direction.Down:
+                                FollowBeam(left, Direction.Right);
+                                break;
+                        }
+                        break;
+                    case '-':
+                        switch (entersFrom)
+                        {
+                            case Direction.Left:
+                                FollowBeam(right, Direction.Left);
+                                break;
+                            case Direction.Right:
+                                FollowBeam(left, Direction.Right);
+                                break;
+                            case Direction.Up:
+                            case Direction.Down:
+                                FollowBeam(left, Direction.Right);
+                                FollowBeam(right, Direction.Left);
+                                break;
+                        }
+                        break;
+                    case '|':
+                        switch (entersFrom)
+                        {
+                            case Direction.Left:
+                            case Direction.Right:
+                                FollowBeam(down, Direction.Up);
+                                FollowBeam(up, Direction.Down);
+                                break;
+                            case Direction.Up:
+                                FollowBeam(down, Direction.Up);
+                                break;
+                            case Direction.Down:
+                                FollowBeam(up, Direction.Down);
+                                break;
+                        }
+                        break;
+                }
+            }
         }
 
         private class Location
@@ -178,12 +251,6 @@ namespace AdventOfCode._2023.Day16
             {
                 return $"{Type} {EnergizedCount}";
             }
-        }
-
-        private class BeamStart
-        {
-            public Direction EntersFrom { get; set; }
-            public Coordinate2D Coordinate { get; set; }
         }
     }
 }
