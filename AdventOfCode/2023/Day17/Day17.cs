6c55ee6c4226c1e11b9a178fa9a2c909b4a8558a
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-20 05:43:33 +0000

    2023 Day 17 WIP

--- AdventOfCode/2023/Day17/Day17.cs
+++ AdventOfCode/2023/Day17/Day17.cs
@@ -5,16 +5,23 @@ namespace AdventOfCode._2023.Day17
 {
     public class Day17 : Day
     {
-        public Day17() : base(2023, 17, "Day17/input_2023_17.txt", "", "", false)
+        public Day17() : base(2023, 17, "Day17/input_2023_17.txt", "", "", true)
         {
 
         }
 
         private Grid2D<Location> _map;
+        private Coordinate2D _start;
+        private Coordinate2D _end;
         public override void Initialise()
         {
             var height = InputLines.Count;
-            _map = new Grid2D<Location>(InputLines[0].Length, height);
+            var width = InputLines[0].Length;
+
+            _start = new Coordinate2D(0, height - 1);
+            _end = new Coordinate2D(width - 1, 0);
+
+            _map = new Grid2D<Location>(width, height);
             foreach (var coordinate in _map.AllCoordinates())
             {
                 var c = InputLines[(int)(height - 1 - coordinate.Y)][(int)coordinate.X];
@@ -30,7 +37,109 @@ namespace AdventOfCode._2023.Day17
 
         public override string Part1()
         {
-            return string.Empty;
+            var end = _map.Read(_end);
+
+            var previousBest = 999_999;
+            var again = true;
+            while (again)
+            {
+                CalculateShortestPaths();
+
+                var shortestPath = end.ShortestPaths.OrderBy(p => p.TotalHeatLoss).First();
+                var minimumHeatLoss = shortestPath.TotalHeatLoss;
+
+                if (minimumHeatLoss < previousBest)
+                {
+                    again = true;
+                    previousBest = minimumHeatLoss;
+                    TraceLine($"New best: {previousBest}");
+                }
+                else
+                {
+                    again = false;
+                }
+            }
+
+
+            foreach (var path in end.ShortestPaths.OrderBy(p => p.TotalHeatLoss))
+            {
+                // DrawPath(path);
+            }
+
+            // 947 - too high (10, 10)
+            // 981 - too high
+            // 1037 - too high
+            return previousBest.ToString();
+        }
+
+        private void DrawPath(Path path)
+        {
+            TraceLine();
+            foreach (var y in _map.YIndexes().OrderByDescending(y => y))
+            {
+                foreach (var x in _map.XIndexes())
+                {
+                    if (path.Steps.Any(s => s.X == x && s.Y == y))
+                    {
+                        Trace("#");
+                    }
+                    else
+                    {
+                        Trace(".");
+                    }
+                }
+                TraceLine();
+            }
+        }
+
+        private void CalculateShortestPaths()
+        {
+            var currentStart = _start;
+            CalculateShortestPaths(currentStart);
+
+            while (currentStart.Y > 0)
+            {
+                currentStart = currentStart.Down();
+
+                var currentPosition = currentStart;
+                CalculateShortestPaths(currentPosition);
+
+                var inGrid = true;
+                while (inGrid)
+                {
+                    currentPosition = currentPosition.Up().Right();
+                    if (_map.IsInGrid(currentPosition))
+                    {
+                        CalculateShortestPaths(currentPosition);
+                    }
+                    else
+                    {
+                        inGrid = false;
+                    }
+                }
+            }
+
+            while (currentStart.X < _map.MaxX)
+            {
+                currentStart = currentStart.Right();
+
+                var currentPosition = currentStart;
+                CalculateShortestPaths(currentPosition);
+
+                var inGrid = true;
+                while (inGrid)
+                {
+                    currentPosition = currentPosition.Up().Right();
+                    if (_map.IsInGrid(currentPosition))
+                    {
+                        CalculateShortestPaths(currentPosition);
+                    }
+                    else
+                    {
+                        inGrid = false;
+                    }
+                }
+            }
         }
 
         public override string Part2()
@@ -38,27 +147,120 @@ namespace AdventOfCode._2023.Day17
             return string.Empty;
         }
 
-        private Path GetShortestPath(Location location)
+        private void CalculateShortestPaths(Coordinate2D coordinate)
         {
-            if (location.ShortestPaths != default)
+            var location = _map.Read(coordinate);
+            /*
+            if (location.ShortestPaths != null)
             {
-                return location.ShortestPaths
-                    .OrderByDescending(x => x.TotalHeatLoss)
-                    .First();
+                TraceLine($"Already Got Shortest Path {location.Coordinate}");
+                return;
+            }
+            */
+            // TraceLine($"Calculating Shortest Path {location.Coordinate}");
+
+            if (location.Coordinate.Equals(_start))
+            {
+                location.ShortestPaths = new List<Path>
+                {
+                    new Path
+                    {
+                        Steps = new List<Coordinate2D>()
+                        {
+                            _start
+                        },
+                        TotalHeatLoss = 0
+                    }
+                };
+
+                return;
             }
 
             var validNeighbours = location.Coordinate.Neighbours()
-                .Where(n => _map.IsInGrid(n))
+                .Where(_map.IsInGrid)
+                .Select(_map.Read)
                 .ToList();
 
-            var shortestPaths = new List<Path>();
+            var paths = location.ShortestPaths ?? new List<Path>();
             foreach (var neighbour in validNeighbours)
             {
-                // var shortestPaths
+                // CalculateShortestPaths(neighbour);
+
+                var neighbourShortestPaths = neighbour.ShortestPaths;
+                if (neighbourShortestPaths != null)
+                {
+                    foreach (var path in neighbourShortestPaths)
+                    {
+                        var pathToHere = new Path
+                        {
+                            Steps = path.Steps.Concat(new[] { location.Coordinate }).ToList(),
+                            TotalHeatLoss = path.TotalHeatLoss + location.HeatLoss
+                        };
+
+                        if (IsValidPath(pathToHere))
+                        {
+                            paths.Add(pathToHere);
+                        }
+                    }
+                }
             }
 
-            return null;
+            location.ShortestPaths = paths
+                .OrderBy(p => p.TotalHeatLoss)
+                .Take(35)
+                // 3 -> 846
+                // 5 -> 841
+                // 10 -> 827
+                // 15 -> 755
+                // 18 -> 810 ?!
+                // 25 -> 738
+                // 35 -> 748
+                .ToList();
         }
+
+        private bool IsValidPath(Path path)
+        {
+            var steps = path.Steps;
+            var lastStepIndex = steps.Count - 1;
+
+
+            if (steps.Count > 2)
+            {
+                if (steps[lastStepIndex].Equals(steps[lastStepIndex-2]))
+                {
+                    return false;
+                }
+            }
+
+            var dX = new List<long>();
+            var dY = new List<long>();
+
+            var startCheckFromIndex = lastStepIndex - 4;
+            if (startCheckFromIndex < 0)
+            {
+                return true;
+            }
+
+            var currentIndex = startCheckFromIndex;
+            while (currentIndex < lastStepIndex)
+            {
+                var currentStep = steps[currentIndex];
+                var nextStep = steps[currentIndex + 1];
+
+                dX.Add(nextStep.X - currentStep.X);
+                dY.Add(nextStep.Y - currentStep.Y);
+
+                currentIndex += 1;
+            }
+
+            if (dX.GroupBy(x => x).Count() == 1 && dY.GroupBy(y => y).Count() == 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
 
         private class Path
         {
