5f6e92716f8f6b77bc5aaebd8293e52a83be18b9
Simon Goodman <simon.goodman84@googlemail.com>
2023-12-28 12:55:24 +0000

    2023 Day 17 Part 1

--- AdventOfCode/2023/Day17/Day17.cs
+++ AdventOfCode/2023/Day17/Day17.cs
@@ -1,11 +1,12 @@
 ﻿using AdventOfCode.Shared;
 using AdventOfCode.Shared.Geometry;
+using AdventOfCode.Shared.Graphs;
 
 namespace AdventOfCode._2023.Day17
 {
     public class Day17 : Day
     {
-        public Day17() : base(2023, 17, "Day17/input_2023_17.txt", "", "", true)
+        public Day17() : base(2023, 17, "Day17/input_2023_17.txt", "698", "", true)
         {
 
         }
@@ -37,108 +38,220 @@ namespace AdventOfCode._2023.Day17
 
         public override string Part1()
         {
-            var end = _map.Read(_end);
-
-            var previousBest = 999_999;
-            var again = true;
-            while (again)
+            var graph = new Graph<NodeData>();
+            foreach (var coordinate in _map.AllCoordinates())
             {
-                CalculateShortestPaths();
+                var heatloss = _map.Read(coordinate).HeatLoss;
+                AddNode(graph, coordinate, Direction.Up, heatloss);
+                AddNode(graph, coordinate, Direction.Down, heatloss);
+                AddNode(graph, coordinate, Direction.Left, heatloss);
+                AddNode(graph, coordinate, Direction.Right, heatloss);
+            }
 
-                var shortestPath = end.ShortestPaths.OrderBy(p => p.TotalHeatLoss).First();
-                var minimumHeatLoss = shortestPath.TotalHeatLoss;
-
-                if (minimumHeatLoss < previousBest)
+            foreach (var coordinate in _map.AllCoordinates())
+            {
+                if (graph.TryGetNode(NodeData.GetIdentifier(coordinate, Direction.Up), out var enteredFromUp))
                 {
-                    again = true;
-                    previousBest = minimumHeatLoss;
-                    TraceLine($"New best: {previousBest}");
+                    var heatloss = 0;
+                    for (var x = coordinate.X - 1; x >= coordinate.X - 3; x -= 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(x, coordinate.Y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Right), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromUp,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+
+                    heatloss = 0;
+                    for (var x = coordinate.X + 1; x <= coordinate.X + 3; x += 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(x, coordinate.Y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Left), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromUp,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
                 }
-                else
+
+                if (graph.TryGetNode(NodeData.GetIdentifier(coordinate, Direction.Down), out var enteredFromDown))
                 {
-                    again = false;
+                    var heatloss = 0;
+                    for (var x = coordinate.X - 1; x >= coordinate.X - 3; x -= 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(x, coordinate.Y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Right), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromDown,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+
+                    heatloss = 0;
+                    for (var x = coordinate.X + 1; x <= coordinate.X + 3; x += 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(x, coordinate.Y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Left), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromDown,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+                }
+
+
+                if (graph.TryGetNode(NodeData.GetIdentifier(coordinate, Direction.Left), out var enteredFromLeft))
+                {
+                    var heatloss = 0;
+                    for (var y = coordinate.Y - 1; y >= coordinate.Y - 3; y -= 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(coordinate.X, y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Up), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromLeft,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+
+                    heatloss = 0;
+                    for (var y = coordinate.Y + 1; y <= coordinate.Y + 3; y += 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(coordinate.X, y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Down), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromLeft,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+                }
+
+                if (graph.TryGetNode(NodeData.GetIdentifier(coordinate, Direction.Right), out var enteredFromRight))
+                {
+                    var heatloss = 0;
+                    for (var y = coordinate.Y - 1; y >= coordinate.Y - 3; y -= 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(coordinate.X, y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Up), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromRight,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
+
+                    heatloss = 0;
+                    for (var y = coordinate.Y + 1; y <= coordinate.Y + 3; y += 1)
+                    {
+                        var destinationCoordinate = new Coordinate2D(coordinate.X, y);
+                        if (graph.TryGetNode(NodeData.GetIdentifier(destinationCoordinate, Direction.Down), out var destinationNode))
+                        {
+                            heatloss += _map.Read(destinationCoordinate).HeatLoss;
+                            graph.AddEdge(new GraphEdge<NodeData>
+                            {
+                                Source = enteredFromRight,
+                                Destination = destinationNode,
+                                Distance = heatloss
+                            });
+                        }
+                    }
                 }
             }
 
+            graph.TryGetNode(NodeData.GetIdentifier(_start, Direction.Right), out var startRight);
+            graph.TryGetNode(NodeData.GetIdentifier(_start, Direction.Down), out var startDown);
 
-            foreach (var path in end.ShortestPaths.OrderBy(p => p.TotalHeatLoss))
+            graph.TryGetNode(NodeData.GetIdentifier(_end, Direction.Left), out var endLeft);
+            graph.TryGetNode(NodeData.GetIdentifier(_end, Direction.Up), out var endUp);
+
+            var shortestPath = int.MaxValue;
+            foreach(var start in new[] { startRight, startDown })
             {
-                // DrawPath(path);
+                foreach (var end in new[] { endLeft, endUp })
+                {
+                    var currentShortestPath = graph.GetShortestPathNodesAndDistance(
+                        start,
+                        end,
+                        (int)(10 * _map.Width * _map.Height));
+
+                    if (currentShortestPath.Distance < shortestPath)
+                    {
+                        shortestPath = currentShortestPath.Distance;
+                    }
+                }
             }
 
-            // 947 - too high (10, 10)
-            // 981 - too high
-            // 1037 - too high
-            return previousBest.ToString();
+            return shortestPath.ToString();
         }
 
-        private void DrawPath(Path path)
+        private void AddNode(
+            Graph<NodeData> graph,
+            Coordinate2D coordinate,
+            Direction direction,
+            int heatloss)
         {
-            TraceLine();
-            foreach (var y in _map.YIndexes().OrderByDescending(y => y))
+            var neighbour = coordinate.Neighbour(direction);
+            if (!_map.IsInGrid(neighbour))
             {
-                foreach (var x in _map.XIndexes())
-                {
-                    if (path.Steps.Any(s => s.X == x && s.Y == y))
-                    {
-                        Trace("#");
-                    }
-                    else
-                    {
-                        Trace(".");
-                    }
-                }
-                TraceLine();
+                return;
             }
+
+            graph.AddNode(new GraphNode<NodeData>(new NodeData
+            {
+                Coordinate = coordinate,
+                EnteringFrom = direction,
+                HeatLoss = heatloss
+            }));
         }
 
-        private void CalculateShortestPaths()
+        private class NodeData : IGraphNodeData
         {
-            var currentStart = _start;
-            CalculateShortestPaths(currentStart);
+            public Coordinate2D Coordinate { get; set; }
+            public Direction EnteringFrom { get; set; }
+            public int HeatLoss { get; set; }
 
-            while (currentStart.Y > 0)
+            public static string GetIdentifier(Coordinate2D coordinate, Direction enteringFrom)
             {
-                currentStart = currentStart.Down();
-
-                var currentPosition = currentStart;
-                CalculateShortestPaths(currentPosition);
-
-                var inGrid = true;
-                while (inGrid)
-                {
-                    currentPosition = currentPosition.Up().Right();
-                    if (_map.IsInGrid(currentPosition))
-                    {
-                        CalculateShortestPaths(currentPosition);
-                    }
-                    else
-                    {
-                        inGrid = false;
-                    }
-                }
+                return $"{coordinate} from {enteringFrom}";
             }
 
-            while (currentStart.X < _map.MaxX)
+            public string GetIdentifier()
             {
-                currentStart = currentStart.Right();
-
-                var currentPosition = currentStart;
-                CalculateShortestPaths(currentPosition);
-
-                var inGrid = true;
-                while (inGrid)
-                {
-                    currentPosition = currentPosition.Up().Right();
-                    if (_map.IsInGrid(currentPosition))
-                    {
-                        CalculateShortestPaths(currentPosition);
-                    }
-                    else
-                    {
-                        inGrid = false;
-                    }
-                }
+                return GetIdentifier(Coordinate, EnteringFrom);
             }
         }
 
@@ -147,132 +260,10 @@ namespace AdventOfCode._2023.Day17
             return string.Empty;
         }
 
-        private void CalculateShortestPaths(Coordinate2D coordinate)
-        {
-            var location = _map.Read(coordinate);
-            /*
-            if (location.ShortestPaths != null)
-            {
-                TraceLine($"Already Got Shortest Path {location.Coordinate}");
-                return;
-            }
-            */
-            // TraceLine($"Calculating Shortest Path {location.Coordinate}");
-
-            if (location.Coordinate.Equals(_start))
-            {
-                location.ShortestPaths = new List<Path>
-                {
-                    new Path
-                    {
-                        Steps = new List<Coordinate2D>()
-                        {
-                            _start
-                        },
-                        TotalHeatLoss = 0
-                    }
-                };
-
-                return;
-            }
-
-            var validNeighbours = location.Coordinate.Neighbours()
-                .Where(_map.IsInGrid)
-                .Select(_map.Read)
-                .ToList();
-
-            var paths = location.ShortestPaths ?? new List<Path>();
-            foreach (var neighbour in validNeighbours)
-            {
-                // CalculateShortestPaths(neighbour);
-
-                var neighbourShortestPaths = neighbour.ShortestPaths;
-                if (neighbourShortestPaths != null)
-                {
-                    foreach (var path in neighbourShortestPaths)
-                    {
-                        var pathToHere = new Path
-                        {
-                            Steps = path.Steps.Concat(new[] { location.Coordinate }).ToList(),
-                            TotalHeatLoss = path.TotalHeatLoss + location.HeatLoss
-                        };
-
-                        if (IsValidPath(pathToHere))
-                        {
-                            paths.Add(pathToHere);
-                        }
-                    }
-                }
-            }
-
-            location.ShortestPaths = paths
-                .OrderBy(p => p.TotalHeatLoss)
-                .Take(35)
-                // 3 -> 846
-                // 5 -> 841
-                // 10 -> 827
-                // 15 -> 755
-                // 18 -> 810 ?!
-                // 25 -> 738
-                // 35 -> 748
-                .ToList();
-        }
-
-        private bool IsValidPath(Path path)
-        {
-            var steps = path.Steps;
-            var lastStepIndex = steps.Count - 1;
-
-
-            if (steps.Count > 2)
-            {
-                if (steps[lastStepIndex].Equals(steps[lastStepIndex-2]))
-                {
-                    return false;
-                }
-            }
-
-            var dX = new List<long>();
-            var dY = new List<long>();
-
-            var startCheckFromIndex = lastStepIndex - 4;
-            if (startCheckFromIndex < 0)
-            {
-                return true;
-            }
-
-            var currentIndex = startCheckFromIndex;
-            while (currentIndex < lastStepIndex)
-            {
-                var currentStep = steps[currentIndex];
-                var nextStep = steps[currentIndex + 1];
-
-                dX.Add(nextStep.X - currentStep.X);
-                dY.Add(nextStep.Y - currentStep.Y);
-
-                currentIndex += 1;
-            }
-
-            if (dX.GroupBy(x => x).Count() == 1 && dY.GroupBy(y => y).Count() == 1)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-
-        private class Path
-        {
-            public List<Coordinate2D> Steps { get; set; }
-            public int TotalHeatLoss { get; set; }
-        }
-
         private class Location
         {
             public Coordinate2D Coordinate { get; set; }
             public int HeatLoss { get; set; }
-            public List<Path> ShortestPaths { get; set; }
 
             public override string ToString()
             {
