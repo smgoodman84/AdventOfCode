7e6df41d3718014a2381112cf8fbff70c20326e7
Simon Goodman <simon.goodman84@googlemail.com>
2024-12-06 06:09:56 +0000

    2024 Day 6 Part 2

--- AdventOfCode/2024/Day06/Day06.cs
+++ AdventOfCode/2024/Day06/Day06.cs
@@ -11,77 +11,48 @@ public class Day06 : Day
 
     }
 
-    private Grid2D<Position> _map;
-    private Coordinate2D _guardPosition;
-    private Direction _guardDirection = Direction.Up;
     public override void Initialise()
     {
-        _map = new Grid2D<Position>(InputLines[0].Length, InputLines.Count);
+    }
+
+    public override string Part1()
+    {
+        var lab = new Lab(InputLines);
+        
+        return lab.GetVisitedCount().ToString();
+    }
+
+    public override string Part2()
+    {
+        var loopCount = 0;
         var y = InputLines.Count - 1;
         foreach (var line in InputLines)
         {
+            TraceLine($"Checking line {y}");
             var x = 0;
             foreach (var c in line)
             {
-                _map.Write(x, y, new Position(c));
-                if (c == '^')
+                var lab = new Lab(InputLines);
+                if (lab.TryBlock(x, y))
                 {
-                    _guardPosition = new Coordinate2D(x, y);
+                    var outcome = lab.GetRouteOutcome();
+                    if (outcome == RouteOutcome.Loop)
+                    {
+                        // TraceLine($"{x},{y} loops");
+                        loopCount += 1;
+                    }
+                    else
+                    {
+                        // TraceLine($"{x},{y} does not loop");
+                    }
                 }
 
                 x += 1;
             }
             y -= 1;
         }
-    }
 
-    private bool MoveNext()
-    {
-        var nextPosition = _guardPosition.Neighbour(_guardDirection);
-        if (!_map.IsInGrid(nextPosition))
-        {
-            TraceLine($"Gone off grid");
-            return false;
-        }
-
-        var nextLocation = _map.Read(nextPosition);
-        if (nextLocation.IsOccupied)
-        {
-            TurnRight();
-            TraceLine($"Turned to face {_guardDirection}");
-            return true;
-        }
-
-        _guardPosition = nextPosition;
-        nextLocation.Visited = true;
-
-        TraceLine($"Now at {nextPosition}");
-        return true;
-    }
-
-    private void TurnRight()
-    {
-        switch (_guardDirection)
-        {
-            case Direction.Up: _guardDirection = Direction.Right; break;
-            case Direction.Right: _guardDirection = Direction.Down; break;
-            case Direction.Down: _guardDirection = Direction.Left; break;
-            case Direction.Left: _guardDirection = Direction.Up; break;
-        }
-    }
-
-    public override string Part1()
-    {
-        while (MoveNext());
-
-        var visitedCount = _map.ReadAll().Count(x => x.Visited);
-
-        return visitedCount.ToString();
-    }
-
-    public override string Part2()
-    {
-        return string.Empty;
+        return loopCount.ToString();
     }
 
     private class Position
@@ -89,11 +60,128 @@ public class Day06 : Day
         public char InitialValue { get; set; }
         public bool IsOccupied { get; set; }
         public bool Visited { get; set; }
+        public List<Direction> VisitDirections = new List<Direction>();
         public Position(char c)
         {
             InitialValue = c;
             IsOccupied = c == '#';
             Visited = c == '^';
+        }
+    }
+
+    private enum RouteOutcome
+    {
+        OnPath,
+        OffGrid,
+        Loop
+    }
+
+    private class Lab
+    {
+        private Grid2D<Position> _map;
+        private Coordinate2D _guardPosition;
+        private Direction _guardDirection = Direction.Up;
+
+        public Lab(List<string> inputLines)
+        {
+            _map = new Grid2D<Position>(inputLines[0].Length, inputLines.Count);
+            var y = inputLines.Count - 1;
+            foreach (var line in inputLines)
+            {
+                var x = 0;
+                foreach (var c in line)
+                {
+                    _map.Write(x, y, new Position(c));
+                    if (c == '^')
+                    {
+                        _guardPosition = new Coordinate2D(x, y);
+                    }
+
+                    x += 1;
+                }
+                y -= 1;
+            }
+        }
+
+        private RouteOutcome MoveNext()
+        {
+            var nextPosition = _guardPosition.Neighbour(_guardDirection);
+            if (!_map.IsInGrid(nextPosition))
+            {
+                // Console.WriteLine($"Gone off grid");
+                return RouteOutcome.OffGrid;
+            }
+
+            var nextLocation = _map.Read(nextPosition);
+            if (nextLocation.IsOccupied)
+            {
+                TurnRight();
+                // Console.WriteLine($"Turned to face {_guardDirection}");
+                return RouteOutcome.OnPath;
+            }
+
+            _guardPosition = nextPosition;
+
+            if (nextLocation.Visited
+             && nextLocation.VisitDirections.Contains(_guardDirection))
+            {
+                return RouteOutcome.Loop;
+            }
+
+            nextLocation.Visited = true;
+            nextLocation.VisitDirections.Add(_guardDirection);
+
+            // Console.WriteLine($"Now at {nextPosition}");
+            return RouteOutcome.OnPath;
+        }
+
+        private void TurnRight()
+        {
+            switch (_guardDirection)
+            {
+                case Direction.Up: _guardDirection = Direction.Right; break;
+                case Direction.Right: _guardDirection = Direction.Down; break;
+                case Direction.Down: _guardDirection = Direction.Left; break;
+                case Direction.Left: _guardDirection = Direction.Up; break;
+            }
+        }
+
+        public RouteOutcome GetRouteOutcome()
+        {
+            while (true)
+            {
+                var outcome = MoveNext();
+                if (outcome != RouteOutcome.OnPath)
+                {
+                    return outcome;
+                }
+            }
+        }
+
+        public int GetVisitedCount()
+        {
+            while (MoveNext() == RouteOutcome.OnPath);
+
+            var visitedCount = _map.ReadAll().Count(x => x.Visited);
+
+            return visitedCount;
+        }
+
+        public bool TryBlock(int x, int y)
+        {
+            var position = _map.Read(x, y);
+            if (position.IsOccupied)
+            {
+                return false;
+            }
+
+            if (_guardPosition.X == x && _guardPosition.Y == y)
+            {
+                return false;
+            }
+
+            position.IsOccupied = true;
+            return true;
         }
     }
 }
