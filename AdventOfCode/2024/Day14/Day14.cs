214c5cc6464add23f55ef6c5eeffca87b7da13fa
Simon Goodman <simon.goodman84@googlemail.com>
2024-12-14 07:12:21 +0000

    2024 Day 14 Part 2

--- AdventOfCode/2024/Day14/Day14.cs
+++ AdventOfCode/2024/Day14/Day14.cs
@@ -1,11 +1,13 @@
 ﻿using AdventOfCode.Shared;
 using AdventOfCode.Shared.Geometry;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace AdventOfCode._2024.Day14;
 
 public class Day14 : Day
 {
-    public Day14() : base(2024, 14, "Day14/input_2024_14.txt", "225943500", "", true)
+    public Day14() : base(2024, 14, "Day14/input_2024_14.txt", "225943500", "6377", true)
     {
     }
 
@@ -63,7 +65,57 @@ public class Day14 : Day
 
     public override string Part2()
     {
-        return string.Empty;
+        var mapWidth = 101;
+        var mapHeight = 103;
+        var result = -1;
+
+        // Found by finding the correct frame from all frames
+        var treeIdentifyingPixels = Enumerable.Range(55, 12)
+            .Select(x => new Coordinate2D(x, 67));
+
+        using Image<Rgba32> allFrames = new(mapWidth, mapHeight, Color.Black);
+
+        var time = 1;
+        while (time < mapWidth * mapHeight)
+        {
+            using Image<Rgba32> image = new(mapWidth, mapHeight, Color.Black);
+            var newPositions = _robots
+                .Select(r => r.GetPositionAfterTime(time, mapWidth, mapHeight))
+                .Distinct()
+                .OrderBy(p => p.Y)
+                .ThenBy(p => p.X)
+                .ToList();
+
+            var robotIndex = 0;
+            for (var y = 0; y < mapHeight; y += 1)
+            {
+                for (var x = 0; x < mapWidth; x += 1)
+                {
+                    if (robotIndex < newPositions.Count
+                        && newPositions[robotIndex].X == x
+                        && newPositions[robotIndex].Y == y)
+                    {
+                        image[x, y] = Color.White;
+                        robotIndex += 1;
+                    }
+                }
+            }
+
+            if (result == -1 
+                && treeIdentifyingPixels.All(p => newPositions.Contains(p)))
+            {
+                image.SaveAsGif("2024_14_2_tree.gif");
+                result = time;
+            }
+
+            allFrames.Frames.AddFrame(image.Frames.RootFrame);
+
+            time += 1;
+        }
+
+        allFrames.SaveAsGif("2024_14_2_all_frames.gif");
+
+        return result.ToString();
     }
 
     private class Robot
