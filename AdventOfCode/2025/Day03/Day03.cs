8acd4c91ea0f748cc0ff3b1b739a65aec528f08d
Simon Goodman <simon.goodman84@googlemail.com>
2025-12-10 07:23:32 +0000

    2025 Day 3 Part 2

--- AdventOfCode/2025/Day03/Day03.cs
+++ AdventOfCode/2025/Day03/Day03.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode._2025.Day03;
 
 public class Day03 : Day
 {
-    public Day03() : base(2025, 3, "Day03/input_2025_03.txt", "16858", "", false)
+    public Day03() : base(2025, 3, "Day03/input_2025_03.txt", "16858", "167549941654721", false)
     {
 
     }
@@ -48,6 +48,49 @@ public class Day03 : Day
 
     public override string Part2()
     {
-        return String.Empty;
+        long total = 0;
+        foreach (var bank in InputLines)
+        {
+            var joltage = MaxJoltage2(bank);
+            TraceLine($"{bank} - {joltage}");
+            total += joltage;
+        }
+
+        return total.ToString();
+    }
+
+    private long MaxJoltage2(string bank)
+    {
+        var digits = bank.Select(c => int.Parse(c.ToString())).ToList();
+        return MaxJoltage2(digits, 12);
+    }
+
+    private long MaxJoltage2(List<int> digits, int batteryCount)
+    {
+        if (batteryCount == 0)
+        {
+            return 0;
+        }
+        
+        var thisDigit = -1;
+        var thisDigitIndex = -1;
+        for (var i = 0; i <= digits.Count - batteryCount; i++)
+        {
+            if (digits[i] > thisDigit)
+            {
+                thisDigit = digits[i];
+                thisDigitIndex = i;
+            }
+        }
+
+        long thisJoltage = thisDigit;
+        for (var i = 1; i < batteryCount; i++)
+        {
+            thisJoltage *= 10;
+        }
+        
+        var additionalJoltage = MaxJoltage2(digits.Skip(thisDigitIndex + 1).ToList(), batteryCount - 1);
+
+        return thisJoltage + additionalJoltage;
     }
 }
