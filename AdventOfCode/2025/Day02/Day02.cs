eaee9ecf523e988549f67e88eeec7aefa59956d9
Simon Goodman <simon.goodman84@googlemail.com>
2025-12-10 06:17:03 +0000

    2025 Day 2 Part 1

---
+++ AdventOfCode/2025/Day02/Day02.cs
@@ -0,0 +1,67 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCode._2025.Day01;
+
+public class Day02 : Day
+{
+    public Day02() : base(2025, 2, "Day02/input_2025_02.txt", "19386344315", "", false)
+    {
+
+    }
+
+    private List<(long Start, long End)> _ranges;
+
+    public override void Initialise()
+    {
+        _ranges = InputLines[0]
+            .Split(',')
+            .Select(r => r.Split('-').Select(long.Parse).ToArray())
+            .Select(r => (r[0], r[1]))
+            .ToList();
+    }
+
+    public override string Part1()
+    {
+        long total = 0;
+        foreach (var range in _ranges)
+        {
+            var invalidNumbers = LongRange(range.Start, range.End)
+                .Where(IsInvalid)
+                .ToList();
+
+            total += invalidNumbers.Sum();
+        }
+
+        return total.ToString();
+    }
+
+    private static IEnumerable<long> LongRange(long start, long end)
+    {
+        var current = start;
+        while (current <= end)
+        {
+            yield return current;
+            current += 1;
+        }
+    }
+    
+    private static bool IsInvalid(long number)
+    {
+        var numString = number.ToString();
+        if (numString.Length % 2 == 1)
+        {
+            return false;
+        }
+
+        var midPoint = numString.Length / 2;
+        var left = numString.Substring(0, midPoint);
+        var right = numString.Substring(midPoint);
+
+        return left == right;
+    }
+
+    public override string Part2()
+    {
+        return string.Empty;
+    }
+}
