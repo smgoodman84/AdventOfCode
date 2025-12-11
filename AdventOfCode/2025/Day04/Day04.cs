c73edb9cab01daf37dba1502cb998c44b01978e7
Simon Goodman <simon.goodman84@googlemail.com>
2025-12-11 06:08:34 +0000

    2025 Day 4 Part 1

---
+++ AdventOfCode/2025/Day04/Day04.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Geometry;
+
+namespace AdventOfCode._2025.Day04;
+
+public class Day04 : Day
+{
+    public Day04() : base(2025, 4, "Day04/input_2025_04.txt", "1527", "", true)
+    {
+
+    }
+
+    private Grid2D<bool> _map;
+    
+    public override void Initialise()
+    {
+        _map = Grid2D<bool>.CreateWithCartesianCoordinates(
+            InputLines, 
+            (coords, c) => c == '@');
+    }
+
+    public override string Part1()
+    {
+        var accessible = 0;
+        foreach (var coord in _map.AllCoordinates())
+        {
+            if (!_map.Read(coord))
+            {
+                continue;
+            }
+            
+            var surroundingPaperCount = 0;
+            foreach (var neighbour in coord.AllNeighbours())
+            {
+                if (_map.IsInGrid(neighbour) && _map.Read(neighbour))
+                {
+                    surroundingPaperCount += 1;
+                }
+            }
+
+            if (surroundingPaperCount < 4)
+            {
+                TraceLine($"{coord} is accessible");
+                accessible += 1;
+            }
+        }
+        return accessible.ToString();
+    }
+
+    public override string Part2()
+    {
+        return string.Empty;
+    }
+}
