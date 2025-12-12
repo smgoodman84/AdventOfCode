97c1eeead3ca8c1c81bae134b57358c2465363a0
Simon Goodman <simon.goodman84@googlemail.com>
2025-12-12 07:41:57 +0000

    2025 Day 6 Part 2

--- AdventOfCode/2025/Day06/Day06.cs
+++ AdventOfCode/2025/Day06/Day06.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode._2025.Day06;
 
 public class Day06 : Day
 {
-    public Day06() : base(2025, 6, "Day06/input_2025_06.txt", "5595593539811", "", false)
+    public Day06() : base(2025, 6, "Day06/input_2025_06.txt", "5595593539811", "10153315705125", false)
     {
 
     }
@@ -52,6 +52,59 @@ public class Day06 : Day
 
     public override string Part2()
     {
-        return string.Empty;
+        var operations = InputLines
+            .Last()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .ToList();
+
+        operations.Reverse();
+
+        var inputs = InputLines.Take(InputLines.Count - 1).ToList();
+        var maxLength = inputs.Max(l => l.Length);
+
+        var transformedInputs = new List<string>();
+        for (var i = maxLength - 1; i >= 0; i--)
+        {
+            var thisInput = "";
+            for (var row = 0; row < inputs.Count; row++)
+            {
+                if (i < inputs[row].Length)
+                {
+                    thisInput += inputs[row][i];
+                }
+            }
+            transformedInputs.Add(thisInput);
+        }
+
+        var results = new List<long>();
+        var inputIndex = 0;
+        foreach (var operation in operations)
+        {
+            long result = 0;
+            if (operation == "*")
+            {
+                result = 1;
+            }
+
+            while (inputIndex < transformedInputs.Count
+                   && !string.IsNullOrWhiteSpace(transformedInputs[inputIndex]))
+            {
+                var input = long.Parse(transformedInputs[inputIndex].Trim());
+                inputIndex += 1;
+                if (operation == "+")
+                {
+                    result += input;
+                }
+                
+                if (operation == "*")
+                {
+                    result *= input;
+                }
+            }
+            inputIndex += 1;
+            results.Add(result);
+        }
+        
+        return results.Sum().ToString();
     }
 }
