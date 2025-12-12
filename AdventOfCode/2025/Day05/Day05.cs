4960f4fc1042d0067a6757f0fddc7aa2d9f35a10
Simon Goodman <simon.goodman84@googlemail.com>
2025-12-12 06:41:00 +0000

    2025 Day 5 Part 1

---
+++ AdventOfCode/2025/Day05/Day05.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Geometry;
+using AdventOfCode.Shared.Numbers;
+
+namespace AdventOfCode._2025.Day05;
+
+public class Day05 : Day
+{
+    public Day05() : base(2025, 5, "Day05/input_2025_05.txt", "558", "", false)
+    {
+
+    }
+
+    private List<NumberRange> _freshRanges = new List<NumberRange>();
+    private List<long> _availableIngredients = new List<long>();
+    
+    public override void Initialise()
+    {
+        var inIngredients = false;
+        foreach (var line in InputLines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                inIngredients = true;
+                continue;
+            }
+
+            if (inIngredients)
+            {
+                _availableIngredients.Add(long.Parse(line));
+                continue;
+            }
+
+            var elements = line.Split("-").Select(long.Parse).ToArray();
+            _freshRanges.AddRange(new NumberRange(elements[0], elements[1]));
+        }
+    }
+    
+    private bool IsFresh(long ingredientId)
+    {
+        return _freshRanges.Any(r => r.Contains(ingredientId));
+    }
+
+    public override string Part1()
+    {
+        var freshCount = _availableIngredients.Count(IsFresh);
+        return freshCount.ToString();
+    }
+
+    public override string Part2()
+    {
+        return string.Empty;
+    }
+}
